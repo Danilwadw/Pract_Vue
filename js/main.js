1bf3e8a84c4f9fa8d47235905cd6b7f4da594bc4
Danilwadw <danilmerpulatov228@gmail.com>
2024-01-16 18:17:42 +0700

    Practical work No. 5

--- js/main.js
+++ js/main.js
@@ -10,18 +10,34 @@ let app = new Vue({
         inventory: 100,
         onSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-
         variants: [{
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
             },
             {
                 variantId: 2235,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
         ],
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
 
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        cart: 0,
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        }
+    },
 
+    updateProduct(variantImage) {
+        this.image = variantImage
+    },
+    methods: {
+        removeFromCart() {
+            this.cart -= 1
+        }
     }
+
 })
